34ed70f306d8e7ca365279ef741ae3b3f61502b5
VonCrowler <daviramos.profissional@gmail.com>
2023-08-02 14:06:09 -0400

    juhui

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worltyufgtyhtyutyugufgud!");
+        System.out.println("Hello worltyufgtyyughuhuihtyutyugufgud!");
     }
 
 
